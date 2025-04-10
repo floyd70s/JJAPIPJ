1c03a629dfc88615b22fe6a24b0e7a0b9cf56dea
claudioperezdiaz <claudioperezdiaz@gmail.com>
2025-04-10 13:11:05 -0400

    puerto 500 para despliegue

--- src/api-casos.py
+++ src/api-casos.py
@@ -1,4 +1,5 @@
 # api-casos.py
+import os
 from flask import Flask
 from routes.corte_suprema import corte_suprema
 
@@ -8,4 +9,5 @@ app = Flask(__name__)
 app.register_blueprint(corte_suprema, url_prefix='/corte_suprema')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
